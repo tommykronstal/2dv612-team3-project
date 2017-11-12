a446830a30d2abbbb1dcc714df74154c2aac5594
AndrasBalla <ab223sq@student.lnu.se>
2017-11-12 15:53:19 +0100

    Implemented company route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,13 @@ const Router = require('express').Router;
 const router = new Router();
 
 const user = require('./model/user/router');
+const company = require('./model/company/router');
 
 router.route('/api').get((req, res) => {
   res.json({ message: 'Welcome to backend API!' });
 });
 
 router.use('/api/user', user);
+router.use('/api/company', company);
 
 module.exports = router;
