c7314fa2b60ce5c9d3407986235620bd57cd7994
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-11-13 10:53:25 +0100

    fix: removed merge issues and added content-routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,36 +1,16 @@
-<<<<<<< HEAD
 import React, {Component} from 'react'
-import './App.css'
 import {connect} from 'react-redux'
-import {updateText} from './actions/example'
-import {MainContentRoutes} from './components/Routes'
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
+import {updateText, updateWelcomeMessage} from './actions/example'
+import {setStatus} from './actions/status'
 
-const MainContentContainer = styled.div`
-  height: 100vh;
-  width: 100wv;
-`
-
-// Made simple palceholder topbar since I do not know where links should be positioned yet.
-const PlaceHolderTopBar = styled.div`
-  height: 64px;
-  width: 100wv;
-`
-=======
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { updateText, updateWelcomeMessage } from './actions/example';
-import { setStatus } from './actions/status';
-
-import Header from './components/Header';
-import Content from './components/Content';
-import Input from './components/Input';
-import Button from './components/Button';
-import Title from './components/Title';
-import Text from './components/Text';
-import StatusModalContainer from './containers/StatusModalContainer';
->>>>>>> d0bd8e3ba4596784f21ae791e9e47f5718ae5ec3
+import Header from './components/Header'
+import Content from './components/Content'
+import Input from './components/Input'
+import Button from './components/Button'
+import Title from './components/Title'
+import Text from './components/Text'
+import StatusModalContainer from './containers/StatusModalContainer'
+import { ContentRoutes } from './components/Routes';
 
 // Maps the state to App component properties
 function mapStateToProps({example}) {
@@ -40,56 +20,42 @@ function mapStateToProps({example}) {
 // Maps functions to update state to properties
 function mapDispatchToProps(dispatch) {
   return {
-<<<<<<< HEAD
+    updateText: text => dispatch(updateText(text)),
+    updateWelcomeMessage: () => dispatch(updateWelcomeMessage()),
+    setStatus: message => dispatch(setStatus(message)),
     updateText: text => dispatch(updateText(text)),
   }
 }
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <PlaceHolderTopBar>
-          <Link to="/login">
-            <p>Login</p>
-          </Link>
-          <Link to="/">
-            <p>Home</p>
-          </Link>
-        </PlaceHolderTopBar>
-        <MainContentContainer>
-          <MainContentRoutes />
-        </MainContentContainer>
-=======
-    updateWelcomeMessage: () => dispatch(updateWelcomeMessage()),
-    setStatus: (message) => dispatch(setStatus(message)),
-    updateText: (text) => dispatch(updateText(text))
-  };
-}
-
-class App extends Component {
-
-  componentDidMount() {
-    this.props.updateWelcomeMessage();
-  }
-
-  render() {
-    // Received from react-redux connect
-    const { text, updateText, message, setStatus } = this.props
+    const {text, updateText, message, setStatus} = this.props
 
     return (
       <div>
         <Header />
         {/* This part should be replaced with react-router */}
         <Content>
-          <Title>Result from server<br/>{message}</Title>
-          <Text center>{!text ? 'This will change when you write.' : text}</Text>
-          <Input type='text' value={text} label='Some text there' onChange={e => updateText(e.target.value)} />
-          <Button primary onClick={() => setStatus(text || 'Success')}>Set Status</Button>
+          <ContentRoutes />
+          <Title>
+            Result from server<br />
+            {message}
+          </Title>
+          <Text center>
+            {!text ? 'This will change when you write.' : text}
+          </Text>
+          <Input
+            type="text"
+            value={text}
+            label="Some text there"
+            onChange={e => updateText(e.target.value)}
+          />
+          <Button primary onClick={() => setStatus(text || 'Success')}>
+            Set Status
+          </Button>
         </Content>
         {/* Until here */}
         <StatusModalContainer />
->>>>>>> d0bd8e3ba4596784f21ae791e9e47f5718ae5ec3
       </div>
     )
   }
