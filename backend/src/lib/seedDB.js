7bd6bb14d285ecbb55d97d8e1ee75e410294b17a
Tommy Kronstal <tommy.kronstal@gmail.com>
2017-11-16 10:51:09 +0100

    adds seed admin to db

---
+++ backend/src/lib/seedDB.js
@@ -0,0 +1,18 @@
+const userFacade = require('../model/user/facade');
+const userController = require('../model/user/controller');
+
+exports.admin = function(adminAccount) {
+    userFacade.findOne({firstName: adminAccount.firstName}).then(doc => {
+
+      if (!doc) {
+
+          let req = {
+              body: adminAccount,
+          };
+
+          userController.register(req);
+
+      }
+    })
+    .catch((e) => console.log('Something went wrong', e));
+  }
