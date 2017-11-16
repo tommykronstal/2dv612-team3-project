a83282babd5b224a17eceda5df11b5843748cfc6
Simon Palmqvist <simon.palmqvist@gmail.com>
2017-11-16 20:34:39 +0100

    Fixing bug with not setting token

--- app/src/components/ProtectedRoute.js
+++ app/src/components/ProtectedRoute.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
-export default ({ auth, userRole, component: Component, ...routeProps }) => {
-
-
-  return (
-  <Route {...routeProps} render={props => {
-    console.log(auth, userRole)
-    return (
+export default ({ auth, userRole, component: Component, ...routeProps }) => (
+  <Route {...routeProps} render={props => (
     auth.isAuthenticated && auth.role === userRole ?
       <Component {...props} /> :
       <Redirect to={{ pathname: '/login' }}/>
-  )}}/>
-)}
+  )}/>
+)
