9b154702758fbe29e123c9914b1e8ca4c267ebe6
Tommy Kronstal <tommy.kronstal@gmail.com>
2017-11-16 11:44:29 +0100

    userFacade will create admin user

--- backend/src/lib/seedDB.js
+++ backend/src/lib/seedDB.js
@@ -1,17 +1,19 @@
+const crypto = require('crypto');
 const userFacade = require('../model/user/facade');
 const userController = require('../model/user/controller');
+const salt = 'jod';
 
 exports.admin = function(adminAccount) {
     userFacade.findOne({firstName: adminAccount.firstName}).then(doc => {
 
       if (!doc) {
+          // TODO: move to schema?
+          adminAccount.password = `${adminAccount.password}${salt}`;
+          adminAccount.password = crypto.createHash('sha256').update(adminAccount.password).digest('hex');
 
-          let req = {
-              body: adminAccount,
-          };
-
-          userController.register(req);
-
+          userFacade.create(adminAccount).then((doc) => {
+              console.log('Added admin', doc);
+          }).catch((e) => console.log(e));
       }
     })
     .catch((e) => console.log(e));
