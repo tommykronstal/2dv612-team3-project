564935a310213a0a2116a8f1d715b9c32a80cc5c
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-11-20 13:42:57 +0100

    fix: added onSubmit functionality

--- app/src/pages/RegisterConsumer.js
+++ app/src/pages/RegisterConsumer.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import CenteredForm from '../components/common/CenteredForm'
 import Title from '../components/common/Title'
 import Input from '../components/common/Input'
+import Content from '../components/common/Content'
 import Button from '../components/common/Button'
 import {connect} from 'react-redux'
 import {REGISTER_CONSUMER} from '../formTypes'
@@ -10,42 +11,49 @@ import {setUserToLoggedIn} from '../actions/auth'
 
 const RegisterConsumer = ({
   updateField,
+  tryRegister,
+  loading: {isLoading},
   form: {email, firstname, lastname, password},
 }) => (
-  <CenteredForm>
-    <Title>Register</Title>
-    <Input
-      type="email"
-      label="E-mail"
-      name="email"
-      value={email}
-      onChange={updateField}
-    />
-    <Input
-      type="text"
-      label="Firstname"
-      name="firstname"
-      value={firstname}
-      onChange={updateField}
-    />
-    <Input
-      type="text"
-      label="Lastname"
-      name="lastname"
-      value={lastname}
-      onChange={updateField}
-    />
-    <Input
-      type="password"
-      label="Pasword"
-      name="password"
-      value={password}
-      onChange={updateField}
-    />
-    <Button primary loading={false}>
-      Register
-    </Button>
-  </CenteredForm>
+  <Content>
+    <form onSubmit={e => {
+      e.preventDefault()
+      tryRegister()
+    }}>
+      <Title>Register</Title>
+      <Input
+        type="email"
+        label="E-mail"
+        name="email"
+        value={email}
+        onChange={updateField}
+      />
+      <Input
+        type="text"
+        label="Firstname"
+        name="firstname"
+        value={firstname}
+        onChange={updateField}
+      />
+      <Input
+        type="text"
+        label="Lastname"
+        name="lastname"
+        value={lastname}
+        onChange={updateField}
+      />
+      <Input
+        type="password"
+        label="Pasword"
+        name="password"
+        value={password}
+        onChange={updateField}
+      />
+      <Button primary loading={isLoading}>
+        Register
+      </Button>
+    </form>
+  </Content>
 )
 
 export default connect(
@@ -56,7 +64,14 @@ export default connect(
   }),
   dispatch => ({
     tryRegister: () =>
-      dispatch(submitForm(REGISTER_CONSUMER, '', setUserToLoggedIn, false)),
+      dispatch(
+        submitForm(
+          REGISTER_CONSUMER,
+          '/api/user/register/consumer',
+          setUserToLoggedIn,
+          false,
+        ),
+      ),
     updateField: ({target: {value, name}}) =>
       dispatch(updateField(REGISTER_CONSUMER, name, value)),
   }),
