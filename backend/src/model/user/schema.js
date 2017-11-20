e14a51c4098d1686baf17b01a1a411f793f73b3d
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-11-20 13:20:41 +0100

    feat: added default values for firstname or lastname

--- backend/src/model/user/schema.js
+++ backend/src/model/user/schema.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  firstName: { type: String },
-  lastName: { type: String },
+  firstName: { type: String, default: '' },
+  lastName: { type: String, default: '' },
   email: { type: String, required: true, unique: true },
   role: { type: String, required: true },
   password: { type: String, required: true }
