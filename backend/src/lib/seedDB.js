78703c2de99e8e5eb32c9621ce61d8d7eaae9017
Silvio Sabo <silvio.sabo@protonmail.com>
2017-11-20 14:42:01 +0100

    moved password hash to schema for create user and login

--- backend/src/lib/seedDB.js
+++ backend/src/lib/seedDB.js
@@ -1,4 +1,3 @@
-const crypto = require('crypto');
 const userFacade = require('../model/user/facade');
 const userController = require('../model/user/controller');
 const salt = 'jod';
@@ -7,9 +6,6 @@ exports.admin = function(adminAccount) {
     userFacade.findOne({firstName: adminAccount.firstName}).then(doc => {
 
       if (!doc) {
-          // TODO: move to schema?
-          adminAccount.password = `${adminAccount.password}${salt}`;
-          adminAccount.password = crypto.createHash('sha256').update(adminAccount.password).digest('hex');
 
           userFacade.create(adminAccount).then((doc) => {
               console.log('Added admin', doc);
