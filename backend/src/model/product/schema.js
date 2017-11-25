ced7a305072cb372641e327acd505ef8643f2937
Silvio Sabo <silvio.sabo@protonmail.com>
2017-11-25 12:43:21 +0100

    added product schema

--- backend/src/model/product/schema.js
+++ backend/src/model/product/schema.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
   articleID: { type: String, required: true, unique: true },
-  name: { type: String, required: true, unique: true },
-  category: { type: String, required: true },  
-  materials:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Material' }]
+  name: { type: String, required: true, unique: true }, 
+  materials:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Material' }],
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' }
 });
 
 
