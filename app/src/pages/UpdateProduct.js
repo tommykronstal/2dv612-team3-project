72ba38380aec763761dbb9d539054b2fa80c24f2
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-11-27 20:05:19 +0100

    feat: update product component

--- app/src/pages/UpdateProduct.js
+++ app/src/pages/UpdateProduct.js
@@ -1,14 +1,97 @@
 import React, {Component} from 'react'
-
-
+import Content from '../components/common/Content'
+import Input from '../components/common/Input'
+import {UPDATE_PRODUCT} from '../formTypes'
+import {updateField} from '../actions/form'
+import {connect} from 'react-redux'
+import Button from '../components/common/Button'
+import FileUpload from '../components/FileUpload'
+import {setStatus} from '../actions/status'
+import {submitForm} from '../actions/form'
 
 class UpdateProduct extends Component {
+	state = {displayWarning: false}
 
-  render() {
-    return (
-      <div>
-        <p>Hello</p>
-      </div>
-    )
-  }
+	handleUpload = e => {
+		e.preventDefault()
+		const {
+			form: {productName, productFile},
+			companyId = 10,
+			productId = 50,
+		} = this.props
+
+		if (!productName || !productFile) {
+			return this.setState(() => ({displayWarning: true}))
+		}
+
+		this.props.tryUpdateProduct(companyId, productId)
+	}
+
+	onFileReady = file => {
+		this.props.dispatch(updateField(UPDATE_PRODUCT, 'productFile', file))
+	}
+
+	render() {
+		const {
+			form: {productName, productFile},
+			loading: {isLoading},
+			updateField: _updateField,
+		} = this.props
+		return (
+			<Content>
+				<form
+					onSubmit={e => this.handleUpload(e)}
+					onChange={() => {
+						if (productName && productFile && this.state.displayWarning) {
+							this.setState(() => ({displayWarning: false}))
+						}
+					}}
+				>
+					<FileUpload
+						accepting="application/pdf"
+						warningMessage="Please provide a PDF."
+						onFileReady={this.onFileReady}
+					/>
+
+					<Input
+						warning={
+							this.state.displayWarning
+								? 'Please select a name and a file.'
+								: null
+						}
+						type="text"
+						label="Material Name"
+						name="productName"
+						value={productName}
+						onChange={_updateField}
+					/>
+					<Button primary loading={isLoading}>
+						Save
+					</Button>
+				</form>
+			</Content>
+		)
+	}
 }
+
+const mapDispatchToProps = dispatch => ({
+	tryUpdateProduct: (companyId, productId) =>
+		dispatch(
+			submitForm(
+				UPDATE_PRODUCT,
+				`api/company/${companyId}/product/${productId}/prodcut`,
+				() => setStatus('Product updated'),
+				true
+			)
+		),
+	updateField: ({target}) =>
+		dispatch(updateField(UPDATE_PRODUCT, target.name, target.value)),
+	dispatch,
+})
+
+const mapStateToProps = ({loading, form}) => ({
+	loading,
+	form: form[UPDATE_PRODUCT] || {},
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(UpdateProduct)
