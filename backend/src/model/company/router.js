c34279ca36dbfb6fe49ac13eb2e6751216f184be
Tommy Kronstal <tommy.kronstal@gmail.com>
2017-11-27 20:05:46 +0100

    saves a file to filesystem

--- backend/src/model/company/router.js
+++ backend/src/model/company/router.js
@@ -1,11 +1,17 @@
 const controller = require('./controller');
 const Router = require('express').Router;
 const router = new Router();
+const multer = require('multer');
+const UPLOAD_PATH = './uploads';
+const upload = multer({ dest: `${UPLOAD_PATH}/` }); // multer configuration
 
 router.route('/')
   .get((...args) => controller.find(...args))
   .post((...args) => controller.registerCompany(...args));
 
+router.route('/:companyid/product/:productid/material')
+  .post(upload.single('pdf'), (...args) => controller.uploadFile(...args))
+  .get((...args) => controller.find(...args));
 
 router.route('/:id')
   .put((...args) => controller.update(...args))
