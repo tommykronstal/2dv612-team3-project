9a18089b594c25137a55f379fdd17bb3ee15e429
LW <lw222ii@student.lnu.se>
2017-11-29 18:40:22 +0800

    companyId back in token

--- backend/src/model/user/controller.js
+++ backend/src/model/user/controller.js
@@ -22,14 +22,15 @@ class UserController extends Controller {
 
         if (match) {
           const role = doc.role;
-          const token = jwt.sign(JSON.stringify({ email, role }), jwtSecret);
           if (role === 'COMPANY_ADMIN' || role === "COMPANY_REP") {
             companyFacade.getCompanyID(doc).then((companyId) => {
-              return res.json({ token,  companyId: companyId, error: false });
+              const token = jwt.sign(JSON.stringify({ email, role, companyId: companyId }), jwtSecret);
+              return res.json({ token, error: false });
             }).catch(e => res.status(401).json(e))
 
           } else {
             // Everything went ok, logging in!
+            const token = jwt.sign(JSON.stringify({ email, role }), jwtSecret);
             return res.json({ token, error: false });
           }
         } else {
