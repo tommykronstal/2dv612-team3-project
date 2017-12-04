172c7c887d096149c525c8050369bce6b2653568
Silvio Sabo <silvio.sabo@protonmail.com>
2017-12-04 20:56:25 +0100

    Fixed updating of rating if user already had rated a material

--- backend/src/model/materialRating/schema.js
+++ backend/src/model/materialRating/schema.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const materialRatingSchema = new Schema({
   userid: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   materialid: { type: mongoose.Schema.Types.ObjectId, ref: 'Material', required: true },
-  rating: { type: Number, required: true, min: 1, max: 5 }
+  rating: { type: Number, required: true }
 });
 
 materialRatingSchema.index({userid: 1, materialid: 1}, {unique: true});
