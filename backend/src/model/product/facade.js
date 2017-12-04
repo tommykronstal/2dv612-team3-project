d964b477a0ee575e4b05f17d785530a2a4e37795
fredriko83 <folep02@student.lnu.se>
2017-12-04 12:27:49 +0100

    added :id to api/products/:id route

--- backend/src/model/product/facade.js
+++ backend/src/model/product/facade.js
@@ -1,6 +1,14 @@
 const Facade = require('../../lib/facade');
 const productSchema = require('./schema');
 
-class ProductFacade extends Facade {}
+class ProductFacade extends Facade {
+
+    findById(...args) {
+        return productSchema
+          .findById(...args)
+          .populate('materials')
+          .exec();
+      }
+}
 
 module.exports = new ProductFacade(productSchema);
