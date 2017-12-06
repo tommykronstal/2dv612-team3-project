f8f027891a55782cbd799fb583fc8d9bd40c4e5f
Simon Palmqvist <simon.palmqvist@gmail.com>
2017-12-06 16:13:49 +0100

    Fixing tests

--- app/src/pact.test.js
+++ app/src/pact.test.js
@@ -1,4 +1,4 @@
-import Pact from 'pact'
+import Pact, {Matchers} from 'pact'
 import path from 'path'
 import {get} from './lib/http'
 import {post} from './lib/http'
@@ -52,29 +52,38 @@ describe('set up pact', () => {
   // LOGIN TESTS
 
   describe('admin can login in', () => {
-    const expected = { error: false, token: "eyJhbGciOiJIUzI1NiJ9.eyJmaXJzdE5hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4ubnUiLCJyb2xlIjoiQURNSU4ifQ.jcQEXVDj3HZADGaLRRCAIRpLr7anqmWY-J6Ms9yUdgE" }
+    const expected = {
+      error: false,
+      token: 'eyJhbGciOiJIUzI1NiJ9.eyJmaXJzdE5hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4ubnUiLCJyb2xlIjoiQURNSU4ifQ.jcQEXVDj3HZADGaLRRCAIRpLr7anqmWY-J6Ms9yUdgE'
+    }
     beforeAll(() => provider.addInteraction({
       uponReceiving: 'login request with admin login',
       withRequest: {
         method: 'POST',
         path: '/api/user/login',
-        headers: { 'Accept': 'application/json' },
+        headers: {
+          'Content-Type': 'application/json'
+        },
         body: { 'email': 'admin@admin.nu', 'password': 'admin' }
       },
       willRespondWith: {
         status: 200,
         headers: { 'Content-Type': 'application/json; charset=utf-8' },
-        body: expected
+        body: {
+          error: false,
+          token: Matchers.somethingLike('eyJhbGciOiJIUzI1NiJ9.eyJmaXJzdE5hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4ubnUiLCJyb2xlIjoiQURNSU4ifQ.jcQEXVDj3HZADGaLRRCAIRpLr7anqmWY-J6Ms9yUdgE')
+        }
       }
     })
     )
 
     it('returns a matching token', async () => {
-      const result = await post(PACT_HOST + '/api/user/login', {headers: {}, body: JSON.stringify({email: 'admin@admin.nu', password: "admin"})})
+      const result = await post(PACT_HOST + '/api/user/login', {headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({email: 'admin@admin.nu', password: 'admin'})})
 
       expect(result).toEqual({...expected, status: 200})
     })
 
+    it('successfully verifies', () => provider.verify())
   })
 
   describe('company rep can login in', () => {
@@ -84,19 +93,22 @@ describe('set up pact', () => {
         withRequest: {
           method: 'POST',
           path: '/api/user/login',
-          headers: { 'Accept': 'application/json' },
+          headers: { 'Content-Type': 'application/json' },
           body: { 'email': 'rep0@samsung.com', 'password': 'password' }
         },
         willRespondWith: {
           status: 200,
           headers: { 'Content-Type': 'application/json; charset=utf-8' },
-          body: expected
+          body: {
+            error: false,
+            token: Matchers.somethingLike('eyJhbGciOiJIUzI1NiJ9.eyJmaXJzdE5hbWUiOiJGTlNhbXN1bmdSZXAwIiwiZW1haWwiOiJyZXAwQHNhbXN1bmcuY29tIiwicm9sZSI6IkNPTVBBTllfUkVQIiwiY29tcGFueUlkIjoiNWEyN2VmYjA1ZGY1ODkzODdjZTY3ZTlhIn0.9yhfCalyanbjmZKwsBfHiCxvhHn5qIv2SuDNvM7D69s')
+          }
         }
       })
     )
 
     it('returns a matching token', async () => {
-      const result = await post(PACT_HOST + '/api/user/login', {headers: {}, body: JSON.stringify({email: 'rep0@samsung.com', password: "password"})})
+      const result = await post(PACT_HOST + '/api/user/login', {headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({email: 'rep0@samsung.com', password: "password"})})
 
       expect(result).toEqual({...expected, status: 200})
     })
@@ -110,57 +122,77 @@ describe('set up pact', () => {
         withRequest: {
           method: 'POST',
           path: '/api/user/login',
-          headers: { 'Accept': 'application/json' },
+          headers: { 'Content-Type': 'application/json' },
           body: { 'email': 'user29@user.com', 'password': 'password' }
         },
         willRespondWith: {
           status: 200,
           headers: { 'Content-Type': 'application/json; charset=utf-8' },
-          body: expected
+          body: {
+            error: false,
+            token: Matchers.somethingLike('eyJhbGciOiJIUzI1NiJ9.eyJmaXJzdE5hbWUiOiJGTnVzZXIyOSIsImVtYWlsIjoidXNlcjI5QHVzZXIuY29tIiwicm9sZSI6IlVTRVIifQ.w2_IERnUUMbnSeGHSjNv0CMIEC-YSA4UMksRXdv5g-8')
+          }
         }
       })
     )
 
     it('returns a matching token', async () => {
-      const result = await post(PACT_HOST + '/api/user/login', {headers: {}, body: JSON.stringify({email: 'user29@user.com', password: "password"})})
+      const result = await post(PACT_HOST + '/api/user/login', {headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({email: 'user29@user.com', password: "password"})})
 
       expect(result).toEqual({...expected, status: 200})
-
-      // todo verify result.token and then verify role?
-
     })
-
   })
 
 
 // TEST 2 START
   describe('returns all products', () => {
-    const expected = {message: 'Welcome to backend API!'}
+    const headers = {
+      'Accept': 'application/json',
+      'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJmaXJzdE5hbWUiOiJGTnVzZXIyOSIsImVtYWlsIjoidXNlcjI5QHVzZXIuY29tIiwicm9sZSI6IlVTRVIifQ.w2_IERnUUMbnSeGHSjNv0CMIEC-YSA4UMksRXdv5g-8'
+    }
+
     beforeAll(() => provider.addInteraction({
         uponReceiving: 'a request for getting products',
         withRequest: {
           method: 'GET',
-          path: '/api/user/products',
-          headers: { 'Accept': 'application/json' }
+          path: '/api/product',
+          headers
         },
         willRespondWith: {
           status: 200,
           headers: { 'Content-Type': 'application/json; charset=utf-8' },
-          body: expected
+          body: Matchers.eachLike({
+            "_id": "5a280388169758001ce1fd6b",
+            "name": "Samsung Mobile Phone 0",
+            "category": "5a280387169758001ce1fd5f",
+            "rating": [],
+            "materials": ["5a280388169758001ce1fd69"]
+          })
         }
       })
     )
 
     it('returns a list of events', async () => {
-      const result = await get(PACT_HOST + '/api/user/products', {headers: { 'Accept': 'application/json' }})
+      const result = await get(PACT_HOST + '/api/product', {headers})
 
-      expect(result).toEqual({...expected, status: 200})
+      const expected = [
+        {
+          "_id": "5a280388169758001ce1fd6b",
+          "name": "Samsung Mobile Phone 0",
+          "category": "5a280387169758001ce1fd5f",
+          "rating": [],
+          "materials": ["5a280388169758001ce1fd69"]
+        }
+      ]
+
+      expected.status = 200 // fix for ugly mapping of response status
+
+      expect(result).toEqual(expected)
     })
     it('successfully verifies', () => provider.verify())
   })
 // TEST 2 END
 
-  // TEST 2 START
   describe('returns an error if no token in header on auth route', () => {
     const expected = {message: 'There was no token in the header', error: true }
     beforeAll(() => provider.addInteraction({
@@ -183,11 +215,7 @@ describe('set up pact', () => {
 
       expect(result).toEqual({...expected, status: 401})
 
-
-
     })
   })
-// TEST 2 END
-
 
 })
