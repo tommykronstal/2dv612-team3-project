c4852df23f7c908bc858813f12e63c006b27a859
Tommy Kronstal <tommy.kronstal@gmail.com>
2017-12-09 14:18:11 +0100

    updates annotation if exists

--- backend/src/model/material/controller.js
+++ backend/src/model/material/controller.js
@@ -1,6 +1,7 @@
 const Controller = require('../../lib/controller');
 const materialFacade = require('./facade');
 const jwt = require('jsonwebtoken');
+const annotationFacade = require('../annotation/facade');
 
 class MaterialController extends Controller {
 
@@ -10,15 +11,28 @@ class MaterialController extends Controller {
     const useremail = jwt.verify(req.headers.authorization, 'keyboardcat').email;
     const material = req.params.id;
 
-    res.status(201).json({error: false, annotation: annotation, user: useremail, material: material});
+    await annotationFacade.find({email: useremail, materialid: material}).then(doc => {
+        console.log(doc);
+        if (!doc.length) {
+            annotationFacade.create({email: useremail, materialid: material, annotation: annotation});
+        } else {
+            const newAnnotation = doc[0].annotation + "\n" + annotation;
+            
+            annotationFacade.update({email: useremail, materialid: material, annotation: newAnnotation});
+        }
+        res.status(201).json({error: false, annotation: annotation, email: useremail, materialid: material});
+    })
+    .catch((e) => { console.log(e) });
 
   }
 
 
   async getAnnotation(req, res, next) {
       const useremail = jwt.verify(req.headers.authorization, 'keyboardcat').email;
-      const annotation = "Will be read from db";
+      
       const material = req.params.id;
+
+      const annotation = await annotationFacade.find({email: useremail, materialid: material});
 
       res.status(200).json({error: false, userinfo: useremail, annotation: annotation, material: material});
   } 
