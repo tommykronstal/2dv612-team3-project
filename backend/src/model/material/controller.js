f3eb0808de03a60fd50b94558b50cda498495a7a
Tommy Kronstal <tommy.kronstal@gmail.com>
2017-12-09 12:05:41 +0100

    adds createAnnotion function in material controller

--- backend/src/model/material/controller.js
+++ backend/src/model/material/controller.js
@@ -1,6 +1,29 @@
 const Controller = require('../../lib/controller');
 const materialFacade = require('./facade');
 
-class MaterialController extends Controller {}
+class MaterialController extends Controller {
+
+  async createAnnotation(req, res, next) {
+    const {
+      annotation
+    } = req.body;
+
+    res.status(201).json({error: false, message: annotation});
+
+    /*
+    try {
+      const newUser = await userFacade.createUser(user); // returns an array on succ. if email is not unique i.e. user already exists execution will catch here
+      const companyExists = await companyFacade.find({name: company.companyName})[0]; // todo no unique identifier for companies in schema so have to find first?
+      if (companyExists) return next({message: 'Company already exists', statusCode: 400});
+      company.admin = newUser;
+      await companyFacade.create(company);
+      return res.status(201).json({ error: false });
+    } catch (e) {
+      return next(e);
+    }*/
+  }
+
+
+}
 
 module.exports = new MaterialController(materialFacade);
