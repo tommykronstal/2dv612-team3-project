a7ae1d106beeee361ef9c4a75e9ffacdb272190b
Tommy Kronstal <tommy.kronstal@gmail.com>
2017-12-10 12:11:25 +0100

    updates an annotation when a change has occured to reflect new content

--- backend/src/model/material/controller.js
+++ backend/src/model/material/controller.js
@@ -13,28 +13,21 @@ class MaterialController extends Controller {
     const material = req.params.id;
 
     await annotationFacade.find({email: useremail, materialid: material}).then(doc => {
-        console.log(doc);
         if (!doc.length) {
             annotationFacade.create({email: useremail, materialid: material, annotation: annotation});
         } else {
-            const newAnnotation = doc[0].annotation + "\n" + annotation;
-            
-            annotationFacade.update({email: useremail, materialid: material, annotation: newAnnotation});
+            annotationFacade.update({email: useremail, materialid: material, annotation: annotation});
         }
+
         res.status(201).json({error: false, annotation: annotation, email: useremail, materialid: material});
     })
     .catch((e) => { console.log(e) });
-
   }
-
 
   async getAnnotation(req, res, next) {
       const useremail = jwt.verify(req.headers.authorization, 'keyboardcat').email;
-      
       const material = req.params.id;
-
       const annotation = await annotationFacade.find({email: useremail, materialid: material});
-
       const materialToReturn = await materialFacade.find({_id: material}).then(doc => {
         doc.annotation = annotation;
         res.status(200).json({error: false, userinfo: useremail, annotation: annotation[0].annotation, material: doc});
