556ec2132e2af3e53be029e7a610c4b0059ad565
Tommy Kronstal <tommy.kronstal@gmail.com>
2017-12-10 12:30:50 +0100

    Moves annotation code to material route

--- backend/src/model/material/router.js
+++ backend/src/model/material/router.js
@@ -6,16 +6,16 @@ router.route('/')
   .get((...args) => controller.find(...args));
 
 router.route('/:id')
-  .put((...args) => controller.update(...args))
-  .get((...args) => controller.findById(...args))
+  .put((...args) => controller.createMaterial(...args))
+  .get((...args) => controller.getMaterial(...args)) // getMaterial -> findById
   .delete((...args) => controller.remove(...args));
 
-
+/* These features are perormed directly towards material... For now...
 router.route('/:id/annotation')
   .post((...args) => controller.createAnnotation(...args))
-  //.put((...args) => controller.update(...args))
-  .get((...args) => controller.getAnnotation(...args))
-  //.delete((...args) => controller.remove(...args));
-
+  .put((...args) => controller.update(...args))
+  .get((...args) => controller.getById(...args))
+  .delete((...args) => controller.remove(...args));
+*/
 
 module.exports = router;
