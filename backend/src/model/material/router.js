52c2a6618a4c2d39f984e0a58d800dba58778e9d
Tommy Kronstal <tommy.kronstal@gmail.com>
2017-12-10 19:25:48 +0100

    adds route for annotation

--- backend/src/model/material/router.js
+++ backend/src/model/material/router.js
@@ -10,12 +10,11 @@ router.route('/:id')
   .get((...args) => controller.getMaterial(...args)) // getMaterial -> findById
   .delete((...args) => controller.remove(...args));
 
-/* These features are perormed directly towards material... For now...
+
 router.route('/:id/annotation')
-  .post((...args) => controller.createAnnotation(...args))
+  .post((...args) => controller.addAnnotation(...args))
   .put((...args) => controller.update(...args))
   .get((...args) => controller.getById(...args))
   .delete((...args) => controller.remove(...args));
-*/
 
 module.exports = router;
