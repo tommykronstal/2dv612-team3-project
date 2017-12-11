42ad40912cb783a2ce0803444c9022d5e9c56678
Tommy Kronstal <tommy.kronstal@gmail.com>
2017-12-11 13:23:56 +0100

    removes unused http methods for /annatation

--- backend/src/model/material/router.js
+++ backend/src/model/material/router.js
@@ -2,19 +2,20 @@ const controller = require('./controller');
 const Router = require('express').Router;
 const router = new Router();
 
+
 router.route('/')
   .get((...args) => controller.find(...args));
 
 router.route('/:id')
-  .put((...args) => controller.createMaterial(...args))
+  .put((...args) => controller.update(...args))
   .get((...args) => controller.getMaterial(...args)) // getMaterial -> findById
   .delete((...args) => controller.remove(...args));
 
 
 router.route('/:id/annotation')
   .post((...args) => controller.addAnnotation(...args))
-  .put((...args) => controller.update(...args))
-  .get((...args) => controller.getById(...args))
-  .delete((...args) => controller.remove(...args));
+  //.put((...args) => controller.update(...args))
+  //.get((...args) => controller.getById(...args))
+  //.delete((...args) => controller.remove(...args));
 
 module.exports = router;
