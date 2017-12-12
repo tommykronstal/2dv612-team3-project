04aad6349ad4c7cf58a8b4c78750cc1d2c0c4623
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-12-12 15:19:59 +0100

    feat: updating store with new annotation when update is saved in db

--- app/src/actions/products.js
+++ app/src/actions/products.js
@@ -1,14 +1,23 @@
-import { FETCH_PRODUCTS, FETCH_PRODUCT, SET_RATING } from './types'
+import {
+	FETCH_PRODUCTS,
+	FETCH_PRODUCT,
+	SET_RATING,
+	UPDATE_PRODUCT_MATERIAL_ANNOTATION,
+} from './types'
 
-export const fetchProducts = () => ({ type: FETCH_PRODUCTS })
+export const fetchProducts = () => ({type: FETCH_PRODUCTS})
 
 export const fetchProduct = productId => ({
 	type: FETCH_PRODUCT,
-	productId
+	productId,
 })
 
 export const setRating = (materialId, rating) => ({
 	type: SET_RATING,
 	materialId,
-	rating
+	rating,
+})
+
+export const updateProductAnnotationMaterial = (productId, annotaion) => ({
+	type: UPDATE_PRODUCT_MATERIAL_ANNOTATION,
 })
