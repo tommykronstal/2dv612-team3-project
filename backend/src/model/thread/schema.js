31414cb7bc1b4161985df7410183d1ea375b3603
sillen102 <silvio.sabo@gmail.com>
2017-12-16 22:23:36 +0100

    fixed bug in thread schema that made the app crash

--- backend/src/model/thread/schema.js
+++ backend/src/model/thread/schema.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const threadSchema = new Schema({
   title: {type: String, unique: true, required: true },
   question: { type: String, required: true },
-  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true }],
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
   date: {type: Date, default: Date.now, required: true }
 });
 
