0fe4d8037fef40fe14703d14a8fb974524615152
sillen102 <silvio.sabo@gmail.com>
2017-12-16 22:01:57 +0100

    fixed bug in thread schema that made the app crash

--- backend/src/model/thread/router.js
+++ backend/src/model/thread/router.js
@@ -2,18 +2,8 @@ const controller = require('./controller');
 const Router = require('express').Router;
 const router = new Router();
 
-<<<<<<< HEAD
 router.route('/forum/:categoryid/thread')
   .get((...args) => controller.find(...args))
   .post((...args) => controller.create(...args));
 
 module.exports = router;
-=======
-
-router.route('/forum/thread')
-  .get((...args) => controller.find(...args))
-  .post((...args) => controller.create(...args));
-
-
-module.exports = router;
->>>>>>> 150355c5553b47bfa7620cf1d04028b9216ef83c
