d2d56913c8d95f4f465fa3f24a29959703357483
sillen102 <silvio.sabo@gmail.com>
2017-12-16 22:42:37 +0100

    stared working on thread route

--- backend/src/model/thread/schema.js
+++ backend/src/model/thread/schema.js
@@ -9,3 +9,39 @@ const threadSchema = new Schema({
 });
 
 module.exports = mongoose.model('Thread', threadSchema)
+
+
+
+/************************************************
+ * Old schema. Unfortunately made the app crash *
+ * **********************************************/
+
+ /*
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const Post = mongoose.model('Post').schema
+
+
+const threadSchema = new Schema({
+  question: {
+    title: {
+      type: String,
+      trim: true,
+      unique: true,
+      required:  [true, 'Must set a thread title'],
+      maxLength: [140, 'Title is too long'], // i took the same values yahoo answers use and we all know they have all the answers. right??
+      minLength: [10, 'Title is too short']
+    }
+  },
+
+  posts: {
+    type: [Post],
+
+  },
+
+  date: {type: Date, default: Date.now }
+
+});
+
+module.exports = mongoose.model('Thread', threadSchema)
+*/
