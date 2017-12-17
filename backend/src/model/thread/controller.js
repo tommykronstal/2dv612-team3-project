c7d4fbbc19176d9b5f9fe6cb472f32b99e94e483
sillen102 <silvio.sabo@gmail.com>
2017-12-17 12:15:29 +0100

    started working on thread controller

--- backend/src/model/thread/controller.js
+++ backend/src/model/thread/controller.js
@@ -1,6 +1,22 @@
 const Controller = require('../../lib/controller');
 const threadFacade = require('./facade');
 
-class threadController extends Controller {}
+class threadController extends Controller {
+
+    async findAll(req, res, next) {
+        
+    }
+
+
+    async findForCategory(req, res, next) {
+
+    }
+
+
+    async createForCategory(req, res, next) {
+
+    }
+
+}
 
 module.exports = new threadController(threadFacade);
--- backend/src/model/thread/router.js
+++ backend/src/model/thread/router.js
@@ -2,8 +2,13 @@ const controller = require('./controller');
 const Router = require('express').Router;
 const router = new Router();
 
+
+router.route('/')
+.get((...args) => controller.findAll(...args));
+
+
 router.route('/:categoryid')
-  .get((...args) => controller.find(...args))
-  .post((...args) => controller.create(...args));
+  .get((...args) => controller.findForCategory(...args))
+  .post((...args) => controller.createForCategory(...args));
 
 module.exports = router;
