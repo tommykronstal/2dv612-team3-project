8e4fac0d973274c1cc1e0c6e828d2d9c81c2f0d5
LW <lw222ii@student.lnu.se>
2017-12-18 20:21:53 +0800

    merge conflicts

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   environment: process.env.NODE_ENV || "dev",
   server: {
-    port: process.env.PORT || 3999
+    port: process.env.PORT || 4000
   },
   mongo: {
     url: process.env.MONGO_DB_URI || "mongodb://localhost/backend"
