caccb3ee9ad49fbd886bd6cbbcf13de99744ebca
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-12-18 15:07:59 +0100

    feat: basic implementation of displaying forum thread answers and question

---
+++ app/src/pages/Thread.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchThread, saveAnswer} from '../actions/forum'
+import Loading from '../components/common/Loading'
+import Header from '../components/common/Header'
+import Headline from '../components/common/Headline'
+import Content from '../components/common/Content'
+import Text from '../components/common/Text'
+import styled from 'styled-components'
+import Answer from '../components/Answer'
+import TextAreaForm from '../components/TextAreaForm'
+import InformationHeader from '../components/common/InformationHeader'
+
+const StyledText = styled(Text)`
+  color: rgb(145, 145, 145) !important;
+`
+
+class Thread extends Component {
+  componentDidMount() {
+    const {postId} = this.props.match.params
+    this.props.getThread(postId)
+  }
+
+  onSaveAnswer(answer) {
+    console.log('Adding an answer...')
+    this.props.saveAnswer({answer, postId: this.props.match.params.postId})
+  }
+
+  render() {
+    const {thread} = this.props
+    return (
+      <Content>
+        {!Object.keys(thread).length ? (
+          <Loading />
+        ) : (
+          <div>
+            <InformationHeader
+              showUnderline
+              fadedTextStyle={'italic'}
+              mainText={`${thread.author} asked -`}
+              fadedText={thread.question}
+            />
+            <div
+              style={{
+                minHeight: 512,
+              }}
+            >
+              {thread.answers.length ? (
+                thread.answers.map((answer, i) => (
+                  <Answer {...answer} key={i} />
+                ))
+              ) : (
+                <Headline>
+                  There are currently no answers for this post, be the first
+                  one!
+                </Headline>
+              )}
+            </div>
+
+            <TextAreaForm
+              placeholderValue="Type thread post here."
+              onClickAction={this.onSaveAnswer.bind(this)}
+            />
+          </div>
+        )}
+      </Content>
+    )
+  }
+}
+
+export default connect(
+  ({forum}) => ({
+    thread: forum.thread,
+  }),
+  dispatch => ({
+    getThread: postId => dispatch(fetchThread(postId)),
+    saveAnswer: answerDetails => dispatch(saveAnswer(answerDetails)),
+  }),
+)(Thread)
