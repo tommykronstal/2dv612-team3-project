7d75cd2aa90190a9ed930ad6ce9cf86ad2f22d44
sillen102 <silvio.sabo@gmail.com>
2017-12-18 22:30:39 +0100

    added update function to thread controller and route

--- backend/src/model/thread/router.js
+++ backend/src/model/thread/router.js
@@ -7,8 +7,11 @@ router.route('/')
 
 router.route('/:id')
     .post((...args) => controller.createThread(...args))
-    .put((...args) => controller.updateThread(...args))
     .get((...args) => controller.findById(...args))
     .delete((...args) => controller.remove(...args));
+
+
+router.route('/thread/:id')
+.put((...args) => controller.updateThread(...args));
 
 module.exports = router;
