8742ed843895e6289bf086990126cda33ace12fb
Simon Palmqvist <simon.palmqvist@gmail.com>
2017-12-21 11:32:14 +0100

    Adding filtering

--- app/src/components/common/Dropdown.js
+++ app/src/components/common/Dropdown.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 const Dropdown = styled.select`
   padding: 8px;
+  margin-bottom: 1rem;
   background: white;
   width: ${props => (props.width ? props.width : 128)}px;
   height: ${props => (props.height ? props.height : 32)}px;
@@ -16,9 +17,9 @@ const DropDownItem = styled.option`
   padding: 8px;
 `
 
-export default ({options, name, onClick, width, height, value}) => (
+export default ({label = 'Select a category', options, name, onClick, width, height, value}) => (
   <Dropdown className="styled-select" name={name} value={value || 'default'} onChange={e => onClick(e)}>
-    <option disabled value='default'>Select a category</option>
+    <option disabled value='default'>{label}</option>
     {options.map(({key, value}) => (
       <DropDownItem key={key} value={key}>
         {value}
