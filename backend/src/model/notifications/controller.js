d2ad684858a43f4e284c2e78a4dbfb712332c5dc
sillen102 <silvio.sabo@gmail.com>
2017-12-22 11:08:04 +0100

    started working on notifications POST route

--- backend/src/model/notifications/controller.js
+++ backend/src/model/notifications/controller.js
@@ -1,6 +1,16 @@
 const Controller = require('../../lib/controller');
 const notificationsFacade = require('./facade');
 
-class notificationsContoller extends Controller {}
+class notificationsContoller extends Controller {
+
+    async findForUser(req, res, next) {
+        
+    }
+
+
+    async removeForUser(req, res, next) {
+
+    }
+}
 
 module.exports = new notificationsContoller(notificationsFacade);
--- backend/src/model/notifications/router.js
+++ backend/src/model/notifications/router.js
@@ -2,8 +2,8 @@ const controller = require('./controller');
 const Router = require('express').Router;
 const router = new Router();
 
-router.route('/')
-  .get((...args) => controller.find(...args))
-  .post((...args) => controller.create(...args));
+router.route('/:id')
+  .get((...args) => controller.findForUser(...args))
+  .post((...args) => controller.removeForUser(...args));
 
 module.exports = router;
