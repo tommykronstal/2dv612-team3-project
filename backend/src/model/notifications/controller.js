6b1d344899995ef2dc224612fe1c6ede6dcba98c
sillen102 <silvio.sabo@gmail.com>
2017-12-22 17:42:19 +0100

    added function for removing a notification to notifications controller

--- backend/src/model/notifications/controller.js
+++ backend/src/model/notifications/controller.js
@@ -15,9 +15,21 @@ class notificationsContoller extends Controller {
     }
 
 
+    /**
+     * Used for removing a notification after a user has seen it and clicked on it
+     * @param {*} req 
+     * @param {*} res 
+     * @param {*} next 
+     */
     async removeForUser(req, res, next) {
         try {
+            let notification = await notificationsFacade.findById(req.param('threadid'));
 
+            if(notification) {
+                notificationsFacade.remove(notification);
+            } else {
+                return next({message: 'Could not find notification,', statusCode: 400});
+            }
         } catch (e) {
             console.log(e);
             return next({message: 'Could not remove notification for user.', statusCode: 400});
