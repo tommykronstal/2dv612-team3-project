206113ef2d383e4b79c0cbda7534ec7b307a435d
sillen102 <silvio.sabo@gmail.com>
2017-12-22 11:27:31 +0100

    started working on notifications POST route

--- backend/src/model/notifications/controller.js
+++ backend/src/model/notifications/controller.js
@@ -1,15 +1,27 @@
 const Controller = require('../../lib/controller');
 const notificationsFacade = require('./facade');
+const userFacade = require('../user/facade');
+const jwt = require('jsonwebtoken');
 
 class notificationsContoller extends Controller {
 
     async findForUser(req, res, next) {
-        
+        try {
+
+        } catch (e) {
+            console.log(e);
+            return next({message: 'Could not find user.', statusCode: 400});
+        }
     }
 
 
     async removeForUser(req, res, next) {
+        try {
 
+        } catch (e) {
+            console.log(e);
+            return next({message: 'Could not find user.', statusCode: 400});
+        }
     }
 }
 
--- backend/src/model/notifications/router.js
+++ backend/src/model/notifications/router.js
@@ -2,6 +2,11 @@ const controller = require('./controller');
 const Router = require('express').Router;
 const router = new Router();
 
+
+router.route('/')
+  .post((...args) => controller.create(...args));
+
+
 router.route('/:id')
   .get((...args) => controller.findForUser(...args))
   .post((...args) => controller.removeForUser(...args));
