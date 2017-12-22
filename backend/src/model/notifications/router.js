570ba9536fc674c543a44ed631fb28d318c5646a
sillen102 <silvio.sabo@gmail.com>
2017-12-22 17:37:19 +0100

    added routes for getting notifications for a user and removing notifications for a user

--- backend/src/model/notifications/router.js
+++ backend/src/model/notifications/router.js
@@ -2,8 +2,10 @@ const controller = require('./controller');
 const Router = require('express').Router;
 const router = new Router();
 
+router.route('/notifications/:id')
+  .get((...args) => controller.findForUser(...args));
+
 router.route('/:id')
-  .get((...args) => controller.findForUser(...args))
   .post((...args) => controller.removeForUser(...args));
 
 module.exports = router;
